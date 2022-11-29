7151b6b05cd04305ee31cea49cdf92c53696fd5a
fagnetva <valentin.fagnet@etu.utc.fr>
2022-11-29 21:48:59 +0100

    Divide pixel.go into plyaground.go

--- pixel.go
+++ pixel.go
@@ -1,23 +1,9 @@
 package main
 
-import "fmt"
-
 type Color string
 
 type Pixel struct {
 	color Color
-}
-
-type Playground struct {
-	length int
-	width  int
-	zone   [][]*Pixel
-}
-
-func NewPixel() *Pixel {
-	return &Pixel{
-		color: "white",
-	}
 }
 
 func (p *Pixel) GetPixelColor() Color {
@@ -28,37 +14,8 @@ func (p *Pixel) SetPixelColor(color Color) {
 	p.color = color
 }
 
-func NewPlayground(l int, w int) *Playground {
-
-	playzone := make([][]*Pixel, l)
-	for i := 0; i < l; i++ {
-		playzone[i] = make([]*Pixel, w)
-		for j := 0; j < w; j++ {
-			playzone[i][j] = NewPixel()
-		}
-	}
-	return &Playground{
-		length: l,
-		width:  w,
-		zone:   playzone,
-	}
-}
-
-func (p *Playground) GetLength() int {
-	return p.length
-}
-
-func (p *Playground) GetWidth() int {
-	return p.width
-}
-
-func main() {
-	myPlay := NewPlayground(100, 100)
-
-	for i := 0; i < myPlay.GetLength(); i++ {
-		for j := 0; j < myPlay.GetWidth(); j++ {
-			fmt.Print(myPlay.zone[i][j].GetPixelColor(), " ")
-		}
-		fmt.Println("")
+func NewPixel() *Pixel {
+	return &Pixel{
+		color: "white",
 	}
 }
