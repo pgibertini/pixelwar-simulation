bae0ebdfa60ec2413505cf03572e6ac123bdd3b3
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-01-05 21:22:37 +0100

    foramt html

--- agent/server/print_canvas_diff.go
+++ agent/server/print_canvas_diff.go
@@ -9,76 +9,77 @@ import (
 const canvasTemplateDiff = `
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>canvas</title>
+    <head>
+        <title>canvas</title>
 
-	<style>
-	  body { background:#eee; margin:1em; text-align:center; }
+        <style>
+            body {
+                background: #eee;
+                margin: 1em;
+                text-align: center;
+            }
 
-      canvas {
-        max-width: 100%;
-        max-height: 100%;
-        width: auto;
-        height: auto;
-        object-fit: contain;
-		border:1px solid #ccc
-      }
-    </style>
+            canvas {
+                max-width: 100%;
+                max-height: 100%;
+                width: auto;
+                height: auto;
+                object-fit: contain;
+                border: 1px solid #ccc;
+            }
+        </style>
+    </head>
+    <body>
+        <canvas id="canvas" width="{{.Width}}" height="{{.Height}}"></canvas>
+        <script>
+            const canvasElement = document.getElementById("canvas");
 
-  </head>
-  <body>
-    <canvas id="canvas" width="{{.Width}}" height="{{.Height}}"></canvas>
-    <script>
-		const canvasElement = document.getElementById('canvas');
-
-      		// Send a request to the server to get the current state of the canvas
-			fetch('/get_canvas', {
-			  method: 'POST',
-			  body: JSON.stringify({'place-id': '{{.PlaceID}}', 'reset-diff': true}),
-			  headers: {
-				'Content-Type': 'application/json',
-			  },
-			})
-		  .then(response => response.json())
-		  .then(canvas => {
-			console.log('grid updated');
-			// Update the displayed image on the canvas
-			const ctx = canvasElement.getContext('2d');
-			for (let y = 0; y < canvas.height; y++) {
-			  for (let x = 0; x < canvas.width; x++) {
-				const canvasElement = document.getElementById('canvas');
-				ctx.fillStyle = canvas.grid[x][y];
-				ctx.fillRect(x, y, 1, 1);
-			  }
-			}
-		  })		  
-		  .then(() => {
-      setInterval(function() {
-        // Send a request to the server to get the current state of the canvas
-        fetch('/get_diff', {
-          method: 'POST',
-          body: JSON.stringify({'place-id': '{{.PlaceID}}'}),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-		.then(response => response.json())
-		.then(response => {
-		  console.log('grid updated');
-		  // Update the displayed image on the canvas
-		  const ctx = canvasElement.getContext('2d');
-		  for (let i = 0; i < response.diff.length; i++) {
-			const pixel = response.diff[i];
-			ctx.fillStyle = pixel.c;
-			ctx.fillRect(pixel.x, pixel.y, 1, 1);
-		  }
-		  return response;
-    	});
-      }, 30); // Update the canvas every second
-	});
-
-	</script>
-  </body>
+            // Send a request to the server to get the current state of the canvas
+            fetch("/get_canvas", {
+                method: "POST",
+                body: JSON.stringify({ "place-id": "{{.PlaceID}}", "reset-diff": true }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((canvas) => {
+                    // Update the displayed image on the canvas
+                    const ctx = canvasElement.getContext("2d");
+                    for (let y = 0; y < canvas.height; y++) {
+                        for (let x = 0; x < canvas.width; x++) {
+                            const canvasElement = document.getElementById("canvas");
+                            ctx.fillStyle = canvas.grid[x][y];
+                            ctx.fillRect(x, y, 1, 1);
+                        }
+                    }
+                    console.log("grid initialized");
+                })
+                .then(() => {
+                    setInterval(function () {
+                        // Send a request to the server to get the current state of the canvas
+                        fetch("/get_diff", {
+                            method: "POST",
+                            body: JSON.stringify({ "place-id": "{{.PlaceID}}" }),
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        })
+                            .then((response) => response.json())
+                            .then((response) => {
+                                // Update the displayed image on the canvas
+                                const ctx = canvasElement.getContext("2d");
+                                for (let i = 0; i < response.diff.length; i++) {
+                                    const pixel = response.diff[i];
+                                    ctx.fillStyle = pixel.c;
+                                    ctx.fillRect(pixel.x, pixel.y, 1, 1);
+                                }
+                                return response;
+                            });
+                    }, 30); // Update the canvas every second
+                });
+        </script>
+    </body>
 </html>
 `
 
