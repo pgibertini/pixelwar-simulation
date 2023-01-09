701180b1bbbdd2e70e9ba160673878c752ba170b
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-01-09 15:09:47 +0100

    main script working

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"gitlab.utc.fr/pixelwar_ia04/pixelwar/agent/server"
 	"gitlab.utc.fr/pixelwar_ia04/pixelwar/launcher"
+	"time"
 )
 
 func main() {
 	// server for canvas requests
-	myServer := server.NewServer("TEST", "127.0.0.1:8080")
+	myServer := server.NewServer("TEST", "127.0.0.1:5555")
 	go myServer.Start()
+
+	time.Sleep(time.Second)
 
 	// Launch the pixel war
 	launcher.LaunchPixelWar(
